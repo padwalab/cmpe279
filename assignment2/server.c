fd98cfe46f171ceca4b4347b2d79fa1fc139ddb1
Abhijeet Padwal <38448205+padwalab@users.noreply.github.com>
2021-11-18 21:23:04 -0800

    assignment 2

---
+++ assignment2/server.c
@@ -0,0 +1,116 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+#include <pwd.h>
+#define PORT 8080
+
+void sigint_handler()
+{
+    printf("Child process exiting: %d\n", getpid());
+    exit(0);
+}
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+
+    if (argc == 2)
+    {
+        struct passwd *pw;
+        if ((pw = getpwnam("nobody")) == NULL)
+        {
+            perror("No such user");
+            exit(EXIT_FAILURE);
+        }
+        printf("Original User id is: %d\n", getuid());
+        if (setuid(pw->pw_uid) != 0)
+        {
+            perror("Failed to change user");
+            exit(EXIT_FAILURE);
+        }
+        printf("New User id is: %d\n", getuid());
+        signal(SIGINT, sigint_handler);
+        server_fd = atoi(argv[1]);
+        while (1)
+        {
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                                     (socklen_t *)&addrlen)) < 0)
+            {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+
+            valread = read(new_socket, buffer, 1024);
+            printf("Read %d bytes: %s\n", valread, buffer);
+            send(new_socket, hello, strlen(hello), 0);
+            printf("Hello message sent\n");
+        }
+    }
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+                   &opt, sizeof(opt)) ||
+        setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    int status;
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        char new_server_fd_str[50];
+        sprintf(new_server_fd_str, "%d", server_fd);
+        char src_file[50];
+        strcpy(src_file, argv[0]);
+        char *args[] = {src_file,
+                        new_server_fd_str,
+                        NULL};
+        execvp(argv[0], args);
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+        printf("Child complete.");
+        exit(0);
+    }
+}
